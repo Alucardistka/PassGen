b99143854dbb1fd7d2d6be1d8b813ca7abd672e3
Alucardistka <Polina-97@list.ru>
2016-09-09 12:47:49 +0600

    Загловок-связка

---
+++ Header.h
@@ -0,0 +1,17 @@
+#include "Lib1.cpp"    //Ссылки на файлы-библиотеки
+#include "Lib2.cpp" 
+#include "Lib3.cpp"
+
+void RUbu();              //Функции для генерации символов из файла "trpoGen.cpp"
+void rubu();
+void Chisl();
+void Englbu(); 
+void englbu();
+void Spec();
+
+void Rand (int x);  
+
+void Polz (int *a);       //Функция для общения с пользователем из файла "trpoPolz.cpp"
+
+void Proverka (int *a,int &s);        //Функции для провери введенных пользователем значений из файла "trpoProv.cpp"
+void QuantLeng(int &number, int &length);
